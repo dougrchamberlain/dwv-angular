27405b824084b4b31091cb38eb5a402741b92faa
dougrchamberlain <d.chamberlain@nextech.com>
2021-05-03 23:35:41 -0400

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
-import { DwvModule } from './dwv/dwv.module';
+import { NgModule } from "@angular/core";
+import { DwvModule } from "./dwv/dwv.module";
 
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [ AppComponent ],
-  imports: [ DwvModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
+  declarations: [AppComponent],
+  imports: [
+    DwvModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    }),
+    FlexLayoutModule
+  ],
   bootstrap: [AppComponent]
 })
-
 export class AppModule {}
